996395cad8ef33e62e993ae403152c843f11967f
Jaden_Furtado <136704191+Jaden-Furtado@users.noreply.github.com>
2025-01-30 11:34:04 +0000

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,4 @@
 console.log(1);
 
-document.body.innerHTML="<img src='https://t4.ftcdn.net/jpg/03/21/43/07/360_F_321430761_qQi0CU9tzI5w1k1vJgdA02LMtXtsXvJE.jpg' style='width:100vw;height:100vh;'>";
+// document.body.innerHTML="<img src='https://t4.ftcdn.net/jpg/03/21/43/07/360_F_321430761_qQi0CU9tzI5w1k1vJgdA02LMtXtsXvJE.jpg' style='width:100vw;height:100vh;'>";
+document.body.innerHTML="<iframe src='https://jaden-furtado.github.io/fb-ui-clone/' style='width:100vw;height:100vh'>";
